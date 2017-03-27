26718e60c972ac56545bebf3761a97d03797209d
Song-gao Tu <tusonggao@163.com>
2017-03-27 19:37:31 +0800

    Update THS_automation.py

--- THS_automation.py
+++ THS_automation.py
@@ -10,6 +10,8 @@ import collections
 import pandas as pd
 import datetime
 
+############################################################################################
+
 try:
 
     from pywinauto import application
