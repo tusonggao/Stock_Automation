67e434346d4f510b91e1a85e37fa80d9e9bf408a
Song-gao Tu <tusonggao@163.com>
2017-04-04 21:22:59 +0800

    Update THS_automation.py

--- THS_automation.py
+++ THS_automation.py
@@ -3,6 +3,8 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
+#############################################################################################
+
 import os.path
 import sys
 import time
