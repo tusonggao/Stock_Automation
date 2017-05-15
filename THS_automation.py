a970d90f8fb99b7996ec237d1c7b024f06299b03
Song-gao Tu <tusonggao@163.com>
2017-05-15 23:31:57 +0800

    Update THS_automation.py

--- THS_automation.py
+++ THS_automation.py
@@ -13,7 +13,6 @@ import datetime
 
 
 ##I am doing this to remind myself not forgeting practicing coding EVERY DAY!!! 
-###############################################################################
 ###I am doing this to remind myself not forgeting practicing coding EVERY DAY!!! 
 
 ###I am doing this to remind myself not forgeting practicing coding EVERY DAY!!! 
